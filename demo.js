8a5327541c56729018505cdbdaf0b461afe86057
Liam Doherty <lfdoherty@gmail.com>
2016-06-28 22:06:53 +0800

    linted demo

--- demo.js
+++ demo.js
@@ -2,22 +2,22 @@
 const glRtt = require('@lfdoherty/gl-rtt')
 const rttDoubleBuffered = require('./rtt_double_buffered')
 
-var createTexture = require("@lfdoherty/gl-texture2d")
-var shell = require("gl-now")()
-var glslify = require("glslify")
+const createTexture = require("@lfdoherty/gl-texture2d")
+const shell = require("gl-now")()
+const glslify = require("glslify")
 
-var displayTexture = require('@lfdoherty/gl-texture2d-display')
-var copyFboToTexture = require('@lfdoherty/gl-fbo-texture2d-copy')
+const displayTexture = require('@lfdoherty/gl-texture2d-display')
+const copyFboToTexture = require('@lfdoherty/gl-fbo-texture2d-copy')
 
-let count = 0
+//let count = 0
 
 let initHandle: glRtt.Handle;
 let rttHandle: rttDoubleBuffered.Handle;
 let originalTexture;
 shell.on("gl-init", function() {
-	let gl = shell.gl
+	const gl = shell.gl
 
-	let dim = [shell.width, shell.height];
+	const dim = [shell.width, shell.height];
 
 	rttHandle = rttDoubleBuffered.create(gl, dim, glslify('./demo.frag'), { float: true});
 	initHandle = glRtt.create(gl, dim, glslify('./demo_fill.frag'), { float: true});
@@ -26,18 +26,18 @@ shell.on("gl-init", function() {
 })
 shell.on("tick", function() {
 	rttHandle.run(uniforms => {
-		uniforms.original_texture = originalTexture.bind(1);
+		uniforms.originalTexture = originalTexture.bind(1);
 	});
 })
-shell.on("gl-render", function(t) {
+shell.on("gl-render", () => {
 	displayTexture(rttHandle.readBuffer.color[0]);
 })
 shell.on('gl-resize', (width, height) => {
 	if (!initHandle) return;
 
-	let gl = shell.gl
+	//const gl = shell.gl
 
-	let newDim = [width, height];
+	const newDim = [width, height];
 	initHandle.shape = newDim
 	rttHandle.shape = newDim
 	initHandle.run(undefined, rttHandle.readBuffer);//just reset the state when we resize
